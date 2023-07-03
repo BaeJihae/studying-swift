8d8ada9170372de06a2d2a0c7ad2a8764c9dafef
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2023-07-03 12:59:55 +0900

    ㅎ

--- codingTest/SpiralArrangementOfIntegers/SpiralArrangementOfIntegers/main.swift
+++ codingTest/SpiralArrangementOfIntegers/SpiralArrangementOfIntegers/main.swift
@@ -9,18 +9,31 @@ import Foundation
 
 func solution(_ n:Int) -> [[Int]] {
     
-    var xAnswer:[Int] = [0,0,0,0]
-    var yAnswer:[Int]
+    var answer : [[Int]] = Array(repeating: Array(repeating: 0,count:n ), count: n)
+    let move : [[Int]] = [[0,1], [1,0], [0,-1], [-1,0]]
     
-    for i in Int.Range(0...3) {
-        yAnswer[i] = xAnswer
+    var x = 0, y = 0
+    var realX = 0, realY = 0
+    var i = 0
+    
+    for k in 1...Int( pow( Double(n), 2.0) ){
+        
+        answer[realX][realY] = k
+        
+        x = realX + move[i][0]
+        y = realY + move[i][1]
+        
+        if x < 0 || x >= n || y < 0 || y >= n || answer[x][y] != 0 {
+            i = (i+1)%4
+            realX += move[i][0]
+            realY += move[i][1]
+            continue
+        }
+        
+        realX = x; realY = y
+        
     }
-    
-    print(yAnswer)
-    
-    return []
+    return answer
 }
 
-func main(){
-    solution(4)
-}
+print(solution(5))
