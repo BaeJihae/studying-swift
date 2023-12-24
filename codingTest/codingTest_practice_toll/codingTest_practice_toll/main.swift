763816a90ecaff350860ba80e0f97f4d8780041f
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2023-12-24 14:14:33 +0900

    12/24

--- codingTest/codingTest_practice_toll/codingTest_practice_toll/main.swift
+++ codingTest/codingTest_practice_toll/codingTest_practice_toll/main.swift
@@ -7,13 +7,6 @@
 
 import Foundation
 
-func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
-    if arr1.count == arr2.count {
-        var sum_a = 0; var sum_b = 0
-        arr1.forEach { sum_a += $0 }
-        arr2.forEach { sum_b += $0 }
-        return sum_a > sum_b ? 1 : (sum_a < sum_b) ? -1 : 0
-    }else {
-        return arr1.count > arr2.count ? 1 : -1
-    }
+func solution(_ my_string:String, _ overwrite_string:String, _ s:Int) -> String {
+    
 }
