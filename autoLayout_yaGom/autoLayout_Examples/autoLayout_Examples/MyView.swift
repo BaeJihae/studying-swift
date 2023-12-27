aafc52849567f6e2200247d3e3b5cc96db1fbad3
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2023-12-27 22:32:06 +0900

    commit

---
+++ autoLayout_yaGom/autoLayout_Examples/autoLayout_Examples/MyView.swift
@@ -0,0 +1,17 @@
+//
+//  MyView.swift
+//  autoLayout_Examples
+//
+//  Created by 배지해 on 12/27/23.
+//
+
+import UIKit
+
+@IndexableBase
+class MyView: UIView {
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+
+}
