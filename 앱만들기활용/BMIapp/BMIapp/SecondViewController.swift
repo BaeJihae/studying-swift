3dde92ed2296ca38111eb173d836f09c606cadfd
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-01-01 23:52:05 +0900

    BMIapp

--- 앱만들기활용/BMIapp/BMIapp/SecondViewController.swift
+++ 앱만들기활용/BMIapp/BMIapp/SecondViewController.swift
@@ -13,6 +13,10 @@ class SecondViewController: UIViewController {
     @IBOutlet weak var adviceLabel: UILabel!
     @IBOutlet weak var backButton: UIButton!
     
+    var bmi: Double?
+    var bmiColor: UIColor?
+    var advice: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,9 +29,19 @@ class SecondViewController: UIViewController {
         bmiNumberLabel.layer.cornerRadius = 8
         bmiNumberLabel.backgroundColor = .gray
         
-        
         backButton.clipsToBounds = true
         backButton.layer.cornerRadius = 5
+        
+        // bmi 데이터 출력
+        guard let bmi = bmi else { return }
+        bmiNumberLabel.text = String(bmi)
+        
+        // bmiNumberLabel 배경 설정
+        bmiNumberLabel.backgroundColor = bmiColor
+        
+        // adviceLable 출력
+        adviceLabel.text = advice
+        
     }
     
     
--- 앱만들기활용/BMIapp/BMIapp/ViewController.swift
+++ 앱만들기활용/BMIapp/BMIapp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController{
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
     
+    var bmi: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         makeUI()
@@ -30,14 +32,16 @@ class ViewController: UIViewController{
         calculateButton.setTitle("BMI 계산하기", for: .normal)
         
         heightTextField.placeholder = "cm단위로 입력해주세요."
+        heightTextField.keyboardType = .numbersAndPunctuation
         
         weightTextField.placeholder = "kg단위로 입력해주세요."
+        weightTextField.keyboardType = .numbersAndPunctuation
     }
     
     @IBAction func calculateButton(_ sender: UIButton) {
-        
-        
-        
+        // Bmi 결과값을 뽑아냄.
+        guard let height = heightTextField.text, let weight = weightTextField.text else { return }
+        bmi = caculateBMI(height: height, weight: weight)
         
     }
     
@@ -50,11 +54,53 @@ class ViewController: UIViewController{
         }
         mainLabel.text = "키와 몸무게를 입력해 주세요"
         mainLabel.textColor = .black
-        return false
+        return true
     }
     
+    // 다음 화면으로 데이터 전달을 위한 함수
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        if segue.identifier == "toSecond" {
+            let secondVC = segue.destination as! SecondViewController
+            // 계산된 결과값을 다음 화면으로 전달
+            secondVC.bmi = self.bmi
+            secondVC.bmiColor = getBackgroundColorAndString().0
+            secondVC.advice = getBackgroundColorAndString().1
+        }
+        
+        // 다음 화면으로 넘어가기 TF 비우기
+        heightTextField.text = ""
+        weightTextField.text = ""
+        
+    }
+    
+    // BMI 계산 메서드
+    func caculateBMI(height: String, weight: String) -> Double {
+        guard let h = Double(height), let w = Double(weight) else { return 0.0 }
+        var bmi = w / ( h * h ) * 10000
+        print("BMI결과값(반올림하기전):\(bmi)")
+        bmi = round( bmi * 10 ) / 10
+        print("BMI결과값(반올림한 후):\(bmi)")
+        return bmi
+    }
+    
+    // 색깔과 문자열 얻는 메서드
+    func getBackgroundColorAndString() -> ( UIColor, String ) {
+        guard let bmi = bmi else { return (UIColor.black, "오류") }
+        switch bmi {
+        case ..<18.6:
+            return ( UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1), "저체중")
+        case 18.6..<23.0:
+            return (UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1), "표준")
+        case 23.0..<25.0:
+            return (UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1), "과체중")
+        case 25.0..<30.0:
+            return (UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1), "중도비만")
+        case 30.0...:
+            return (UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1), "고도비만")
+        default:
+            return (UIColor.black, "오류")
+        }
     }
     
 }
