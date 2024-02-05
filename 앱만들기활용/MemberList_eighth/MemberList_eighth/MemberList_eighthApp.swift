c8c3d1e12869da129598ddda2e4a154974ef49d2
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-02-05 15:59:19 +0900

    create MemberListApp.project

---
+++ 앱만들기활용/MemberList_eighth/MemberList_eighth/MemberList_eighthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemberList_eighthApp.swift
+//  MemberList_eighth
+//
+//  Created by 배지해 on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MemberList_eighthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
