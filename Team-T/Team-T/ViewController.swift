bf733acb804f21a9d48665a766c9c5f6acd04eea
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-02-27 15:48:29 +0900

    create Team-T project

---
+++ Team-T/Team-T/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team-T
+//
+//  Created by 배지해 on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
